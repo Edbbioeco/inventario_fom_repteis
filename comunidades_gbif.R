68bd02c0eaad38103f376315bef48964650406d8
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-14 21:07:08 -0300

    Ajustar a correção de taxonomia

--- comunidades_gbif.R
+++ comunidades_gbif.R
@@ -76,6 +76,8 @@ gbif_sf_fom |>
 gbif_sf_fom <- gbif_sf_fom |> 
   dplyr::mutate(species = dplyr::case_match(
     species,
+    
+    "Chelonoidis carbonaria" ~ "Chelonoidis carbonarius",
     "Philodryas aestivus" ~ "Philodryas aestiva",
     "Placosoma glabelum" ~ "Placosoma glabellum",
     "Bothrops alternatus neuw." ~ "Bothrops alternatus",
@@ -90,7 +92,7 @@ gbif_sf_fom <- gbif_sf_fom |>
     "Tomodon dorsatum" ~ "Tomodon dorsatus",
     "Tupinambis merianae" ~ "Salvator marianae",
     "Mabuya frenata" ~ "Notomabuya frenata",
-    "Anisiolepis grilli"  ~ "Urostrophus grilli",
+    c("Anisiolepis grilli", "Anisolepis grilli")  ~ "Urostrophus grilli",
     "Mabuya dorsivittata" ~ "Aspronema dorsivittatum",
     "Sibynomorphus neuwiedi" ~ "Dipsas neuwiedi",
     "Liotyphlops beui" ~ "Liotyphlops ternetzii",
@@ -107,7 +109,7 @@ gbif_sf_fom <- gbif_sf_fom |>
     "Atractus taeniatus" ~ "Atractus paraguayensis",
     "Crotalus durissus terrificus" ~ "Crotalus durissus",
     "Echinanthera affinis" ~ "Dibernardia affinis",
-    "Bothrops newwiedi" ~ "Bothrops neuwiedii",
+    "Bothrops neuwiedi" ~ "Bothrops neuwiedii",
     "Amphisbaena darwini" ~ "Amphisbaena darwinii",
     "Anops kingii" ~ "Amphisbaena kingii",
     "Amphisbaena mertensi" ~ "Amphisbaena mertensii",
