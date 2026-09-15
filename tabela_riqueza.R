d87e12968f48be7846ca6e8930209297dae1c66b
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-15 19:18:45 -0300

    Criar estrutura da tabela

--- tabela_riqueza.R
+++ tabela_riqueza.R
@@ -30,3 +30,20 @@ composicao <- purrr::map_dfr(
 composicao
 
 composicao |> dplyr::glimpse()
+
+# Tabela ----
+
+## Montar a tabela ----
+
+tabela <- composicao |> 
+  dplyr::select(-c(1, 4)) |> 
+  dplyr::mutate(Source = dplyr::case_match(
+    Source,
+    "gbif" ~ "GBIF",
+    "inaturalist" ~ "iNaturalist",
+    .default = Source |> stringr::str_to_title())) |> 
+  dplyr::group_by(Family, Especies) |>  
+  dplyr::summarise(Source = paste(unique(Source), collapse = ", "), 
+                   .groups = "drop")
+
+tabela
