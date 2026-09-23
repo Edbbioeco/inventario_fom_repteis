844036a72b5c93bdf3e0ff8b3f82392dcdf4fc9a
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-23 10:37:20 -0300

    Ajustar a taxonomia

--- comunidades_levantamento.R
+++ comunidades_levantamento.R
@@ -128,7 +128,7 @@ sps_id <- sps_trat |>
                   "Mabuya frenata" ~ "Notomabuya frenata",
                   c("Anisiolepis grilli", "Anisolepis grilli")  ~ "Urostrophus grilli",
                   "Mabuya dorsivittata" ~ "Aspronema dorsivittatum",
-                  "Sibynomorphus neuwiedi" ~ "Dipsas neuwiedi",
+                  "Sibynomorphus neuwiedii" ~ "Dipsas neuwiedi",
                   c("Liotyphlops beui", "Liotyphlops sousai") ~ "Liotyphlops ternetzii",
                   "Taeniophallus poecilopogon" ~ "Dibernardia poecilopogon",
                   "Amphisbaena darwini trachura" ~ "Amphisbaena darwinii",
