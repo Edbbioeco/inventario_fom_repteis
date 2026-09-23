3ab14c7b17fef9998c182539c635461c85ae932e
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-23 10:35:30 -0300

    Ajustar a taxonomia

--- comunidades_gbif.R
+++ comunidades_gbif.R
@@ -94,7 +94,7 @@ gbif_sf_fom <- gbif_sf_fom |>
                   "Mabuya frenata" ~ "Notomabuya frenata",
                   c("Anisiolepis grilli", "Anisolepis grilli")  ~ "Urostrophus grilli",
                   "Mabuya dorsivittata" ~ "Aspronema dorsivittatum",
-                  "Sibynomorphus neuwiedi" ~ "Dipsas neuwiedi",
+                  "Sibynomorphus neuwiedii" ~ "Dipsas neuwiedi",
                   c("Liotyphlops beui", "Liotyphlops sousai") ~ "Liotyphlops ternetzii",
                   "Taeniophallus poecilopogon" ~ "Dibernardia poecilopogon",
                   "Amphisbaena darwini trachura" ~ "Amphisbaena darwinii",
