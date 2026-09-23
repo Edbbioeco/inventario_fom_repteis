eb759b4fb589ba91528dc417a11eaeaa10b84fa4
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-23 10:38:23 -0300

    Ajustar a taxonomia

--- comunidades_specieslink.R
+++ comunidades_specieslink.R
@@ -103,7 +103,7 @@ specieslink_sf_fom <- specieslink_sf_fom |>
                   "Mabuya frenata" ~ "Notomabuya frenata",
                   c("Anisiolepis grilli", "Anisolepis grilli")  ~ "Urostrophus grilli",
                   "Mabuya dorsivittata" ~ "Aspronema dorsivittatum",
-                  "Sibynomorphus neuwiedi" ~ "Dipsas neuwiedi",
+                  "Sibynomorphus neuwiedii" ~ "Dipsas neuwiedi",
                   c("Liotyphlops beui", "Liotyphlops sousai") ~ "Liotyphlops ternetzii",
                   "Taeniophallus poecilopogon" ~ "Dibernardia poecilopogon",
                   "Amphisbaena darwini trachura" ~ "Amphisbaena darwinii",
